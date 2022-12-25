be98d81ad58b27694a9f9470a7b8b864d964c4ab
Jacob Zwicker <75599461+JacobZwicker71@users.noreply.github.com>
2022-12-25 05:36:07 +0000

    push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //don't judge, i just startedish
 //dw I got no idea what you are doing
 //sick
+//want me to explain shit?
 
 var express = require('express');
 var app = express();
@@ -28,7 +29,7 @@ app.get('/login', (req, res) => {
     data = data + ''
     if (err) {
       fs.writeFile(name + '.txt', `password: ${password}`, (err, data) => {
-        if (err) {console.log('New account err: ' + err)}
+        if (err) {console.log('New account err: ' + err)}//something's fucked | (eli)borate? ha-lirious | the client doesn't register the creation of a new 'bottle' | is it because you are trying to put it in a txt file with the name of ... "name".txt? should'nt it just be that Rye.txt and then you input name?| mebe | this.responseText is the data inside the "bottle" | where do you declare responseText? (ps we need a better way of communicating) | look in the chat
       })
     }
     else if (password == 'password') {

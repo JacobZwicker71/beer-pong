70743b3fb489e3589b57b0e5726d0c9278be7013
Jacob Zwicker <75599461+JacobZwicker71@users.noreply.github.com>
2022-12-25 19:51:27 +0000

    m

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ function uniquify (uname) {
 
 function alphanum (num) {
   var anum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']
-  return(anum)
+  return anum[num - 1]
 }

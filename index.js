4038ba1b8d8fe47970e0bcd3513b13e44709874d
Jacob Zwicker <75599461+JacobZwicker71@users.noreply.github.com>
2022-12-25 05:57:05 +0000

    fixed the user creation error

--- index.js
+++ index.js
@@ -10,32 +10,33 @@ var fs = require('fs');
 var server = require('http');
 var { Server } = require('socket.io')
 var io = new Server(server)
-	
+
 app.get('/', (req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      console.log(err + '')
-    }
-    else {
-      res.send(data)
-    }
-  })
+	fs.readFile('index.html', (err, data) => {
+		if (err) {
+			console.log(err + '')
+		}
+		else {
+			res.send(data)
+		}
+	})
 })
 
 app.get('/login', (req, res) => {
-  var name = req.url.split('?')[1].split('+')[0]
-  var password = req.url.split('+')[1]
-  fs.readFile(name + '.txt', (err, data) => {
-    data = data + ''
-    if (err) {
-      fs.writeFile(name + '.txt', `password: ${password}`, (err, data) => {
-        if (err) {console.log('New account err: ' + err)}//something's fucked | (eli)borate? ha-lirious | the client doesn't register the creation of a new 'bottle' | is it because you are trying to put it in a txt file with the name of ... "name".txt? should'nt it just be that Rye.txt and then you input name?| mebe | this.responseText is the data inside the "bottle" | where do you declare responseText? (ps we need a better way of communicating) | look in the chat
-      })
-    }
-    else if (password == 'password') {
-      res.send(data.substr(data.split('\n')[0].length, data.length))
-    }
-  })
+	var name = req.url.split('?')[1].split('+')[0]
+	var password = req.url.split('+')[1]
+	fs.readFile("userData/" + name + '.txt', (err, data) => {
+		data = data + ''
+		if (err) {
+			console.log('')
+			fs.writeFile("userData/" + name + '.txt', `password: ${password}`, (err, data) => {
+				if (err) { console.log('New account err: ' + err) }
+			})
+		}
+		else if (password == data.split('\n')[0]) {//this line is fucked
+			res.send(data.substr(data.split('\n')[0].length, data.length))//this one is too
+		}
+	})
 })
 
 app.listen(1080);
